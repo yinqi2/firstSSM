ec1c3f22189ff87d1e43ddef10e0b509962059e0
yinqi <1204623070@qq.com>
2018-06-22 12:59:46 +0800

    pp

--- firstMavenProject/src/main/java/com/yinqi/service/impl/UserServiceImpl.java
+++ firstMavenProject/src/main/java/com/yinqi/service/impl/UserServiceImpl.java
@@ -31,6 +31,7 @@ public class UserServiceImpl implements UserService {
         return userMapper.insertUser(user);
     }
 
+    //根据名称查找
     public List<User> findByname(String username) {
     	
         return userMapper.queryByname(username);
