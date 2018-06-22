9f9e3d9ea9590ba12e282dc966b097c29d6ceb1d
yinqi <1204623070@qq.com>
2018-06-22 12:36:08 +0800

    jj

--- firstMavenProject/src/main/java/com/yinqi/service/impl/UserServiceImpl.java
+++ firstMavenProject/src/main/java/com/yinqi/service/impl/UserServiceImpl.java
@@ -8,17 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 /**
  * user服务bean
  * @author 尹棋
  * 日期： 2018年6月22日
- *
  */
 @Service("userService")
 public class UserServiceImpl implements UserService {
 
-	//根据类型自动转入
+	 //根据类型自动转入
     @Autowired
     protected UserMapper userMapper;
 
